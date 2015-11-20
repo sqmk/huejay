f77410ab5b25670099717e2380c346d9097d1526
Michael K. Squires <sqmk@php.net>
2015-11-19 22:52:37 -0600

    GitHub #13 - Add example for managing sensor

---
+++ examples/manage-sensor.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+'use strict';
+
+let huejay = require('../lib/Huejay');
+let credentials = require('./.credentials.json');
+
+let client = new huejay.Client(credentials);
+
+console.log('Creating sensor...');
+
+let sensor = new huejay.Sensor;
+sensor.name             = 'Sensor name here';
+sensor.modelId          = 'Sensormodel';
+sensor.softwareVersion  = '1.2.3';
+sensor.type             = 'CLIPGenericFlag';
+sensor.uniqueId         = '123.456.789';
+sensor.manufacturer     = 'Huejay';
+
+sensor.config.on  = false;
+sensor.state.flag = true;
+
+client.createSensor(sensor)
+  .then(sensor => {
+    console.log(`New sensor [${sensor.id}]: ${sensor.name}`);
+    console.log(`  Type: ${sensor.type}`);
+    console.log(`  Model Id: ${sensor.modelId}`);
+    console.log(`  Manufacturer: ${sensor.manufacturer}`);
+    console.log(`  Software Version: ${sensor.softwareVersion}`);
+    console.log(`  Unique Id: ${sensor.uniqueId}`);
+    console.log(`  Config:`);
+    for (let key in sensor.config) {
+      console.log(`    ${key}: ${sensor.config[key]}`);
+    }
+
+    console.log(`  State:`);
+    for (let key in sensor.state) {
+      console.log(`    ${key}: ${sensor.state[key]}`);
+    }
+
+    return sensor.id;
+  })
+  .then(sensorId => {
+    console.log(`Retrieving sensor ${sensorId}`);
+
+    return client.getSensor(sensorId);
+  })
+  .then(sensor => {
+    console.log('Updating sensor and changing state');
+
+    sensor.name = 'New sensor name here';
+
+    sensor.config.on  = false;
+    sensor.state.flag = true;
+
+    return client.saveSensor(sensor);
+  })
+  .then(sensor => {
+    console.log('Deleting sensor');
+
+    return client.deleteSensor(sensor);
+  })
+  .catch(error => {
+    console.log(error.stack);
+  });
