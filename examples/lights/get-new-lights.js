d5ac0f376f6c8f8a242e81eda496e47e721bcdee
Michael K. Squires <sqmk@php.net>
2015-12-15 11:10:38 -0600

    GitHub #23 - Output light info as part of get new lights example

--- examples/lights/get-new-lights.js
+++ examples/lights/get-new-lights.js
@@ -10,7 +10,30 @@ console.log();
 client.lights.getNew()
   .then(lights => {
     for (let light of lights) {
-      console.log(light);
+      console.log(`Light [${light.id}]: ${light.name}`);
+      console.log(`  Type:             ${light.type}`);
+      console.log(`  Unique ID:        ${light.uniqueId}`);
+      console.log(`  Manufacturer:     ${light.manufacturer}`);
+      console.log(`  Model Id:         ${light.modelId}`);
+      console.log('  Model:');
+      console.log(`    Id:             ${light.model.id}`);
+      console.log(`    Manufacturer:   ${light.model.manufacturer}`);
+      console.log(`    Name:           ${light.model.name}`);
+      console.log(`    Type:           ${light.model.type}`);
+      console.log(`    Color Gamut:    ${light.model.colorGamut}`);
+      console.log(`    Friends of Hue: ${light.model.friendsOfHue}`);
+      console.log(`  Software Version: ${light.softwareVersion}`);
+      console.log('  State:');
+      console.log(`    On:         ${light.on}`);
+      console.log(`    Reachable:  ${light.reachable}`);
+      console.log(`    Brightness: ${light.brightness}`);
+      console.log(`    Color mode: ${light.colorMode}`);
+      console.log(`    Hue:        ${light.hue}`);
+      console.log(`    Saturation: ${light.saturation}`);
+      console.log(`    X/Y:        ${light.xy[0]}, ${light.xy[1]}`);
+      console.log(`    Color Temp: ${light.colorTemp}`);
+      console.log(`    Alert:      ${light.alert}`);
+      console.log(`    Effect:     ${light.effect}`);
       console.log();
     }
   })
